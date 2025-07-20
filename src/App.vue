3907d44811363ae330c7a4a4502ec09a0ae630dc
john3volta <workbox_1@tutanota.com>
2025-07-20 15:36:04 +0200

    feat: add App.vue structure with AccountForm

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <AccountForm />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import AccountForm from './components/AccountForm.vue'
+</script>
