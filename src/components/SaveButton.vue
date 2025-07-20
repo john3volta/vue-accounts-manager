3d7388e53f1539c6b080c185b689334bf4bb050e
john3volta <workbox_1@tutanota.com>
2025-07-20 15:50:25 +0200

    feat: add SaveButton component

---
+++ src/components/SaveButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    @click="$emit('save')"
+    color="success"
+    variant="text"
+    size="small"
+    icon
+  >
+    <v-icon>mdi-content-save</v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  save: []
+}>()
+</script> 
