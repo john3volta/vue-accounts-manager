144f520e626ab0fabe77b2e6dc64bd98807b0f2d
john3volta <workbox_1@tutanota.com>
2025-07-20 14:49:36 +0200

    refactor: remove SCSS files and imports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import './styles/main.scss'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
