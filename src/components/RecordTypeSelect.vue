3f95c09b04ab3da8ea1f771967cbfbc594ac38a2
john3volta <workbox_1@tutanota.com>
2025-07-20 16:51:39 +0200

    feat: add RecordTypeSelect component

---
+++ src/components/RecordTypeSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-select
+    :model-value="modelValue"
+    @update:model-value="$emit('update:model-value', $event)"
+    :items="recordTypes"
+    variant="outlined"
+    density="compact"
+    hide-details
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: string
+}>()
+
+defineEmits<{
+  'update:model-value': [value: string]
+}>()
+
+const recordTypes = ['Локальная', 'LDAP']
+</script>
