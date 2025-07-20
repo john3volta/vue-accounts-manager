ab590b035f299ba5fc12c02ec406aa2ee56028c4
john3volta <workbox_1@tutanota.com>
2025-07-20 15:36:49 +0200

    feat: add DeleteButton component

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    @click="$emit('delete')"
+    color="error"
+    variant="text"
+    size="small"
+    icon
+  >
+    <v-icon>mdi-delete</v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  delete: []
+}>()
+</script>
